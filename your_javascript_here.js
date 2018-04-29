151b54f663054ee078986274d53823074ecb999f
Bahar Salbacak <bahar@argumentenfabriek.nl>
2018-04-29 19:25:24 +0200

    assignment 2

--- your_javascript_here.js
+++ your_javascript_here.js
@@ -1,5 +1,36 @@
-// Variables
-
+var hero = {
+  name: "Kita",
+  heroic: true,
+  inventory: ['hamer', 'arrows', 'mirror', 'rope'],
+  health: 20,
+  weapon: { type: 'bow and arrow', damage: 2 }
+};
 // Game logic
+
+function rest(creature) {
+  var creature = {
+  name: "Veras",
+  heroic: false,
+  inventory: ['poison', 'knife', 'flashbang', 'smoke bomb'],
+  health: ++10,
+  weapon: { type: 'axe', damage: 2 }
+  }
+
+  return creature
+};
+
+function pickUpItem(creature, item) {
+  var creatureItem = {
+  item: creature.inventory.push("grenade");
+  }
+  return creature
+};
+
+
+
+
+
+
+
 
 // UI
